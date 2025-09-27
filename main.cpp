f2f234d9f1ff4e0ca8fe179b9226c052fe2f43d1
definitly486 <fg8989@gmail.com>
2025-09-27 12:29:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    int screenWidth = QApplication::desktop()->screenGeometry().width();
+    int screenHeight = QApplication::desktop()->screenGeometry().height();
+    int windowWidth = w.width();
+    int windowHeight = w.height();
+    int x = (screenWidth - windowWidth) / 2;
+    int y = (screenHeight - windowHeight) / 2;
+    w.move(x, y);
     w.show();
     return a.exec();
 }
